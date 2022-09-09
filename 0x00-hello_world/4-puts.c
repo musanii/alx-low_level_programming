8d0c12984279003981584e66b56b8333c2849ad8
Kevin Musanii <kibetmusanii@gmail.com>
2022-09-08 18:41:37 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
-#include<stdio.h>                                                                            
-                                                                                             
+#include<stdio.h>                                                                                                     
 /**                                                                                          
- *  *main - Entry point                                                                         
- *   *Return: ALways 0 (Success)                                                                 
- *    */                                                                                          
+ * main - Entry point                                                                      
+ *   
+ * Return: ALways 0 (Success)                                                                 
+ */                                                                                          
 int main(void)                                                                               
 {                                                                                            
 	puts("\"Programming is like building a multilingual puzzle");
