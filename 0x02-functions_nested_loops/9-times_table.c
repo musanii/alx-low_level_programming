76d7ee03ff7da69a1b169af4bb611eefa3a808fb
Kevin Musanii <kibetmusanii@gmail.com>
2022-09-14 20:10:19 -0700

    timetable fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,9 +22,9 @@ void times_table(void)
 			_putchar(',');
 			_putchar(' ');
 
-			prod = num * mult;
-			
-			if (prod <= 9)
+			_putchar((prod % 10) + '0');
+
+			f (prod <= 9)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
