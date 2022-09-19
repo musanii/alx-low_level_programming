d431cd9bcdb04112b21580157870c2fdae7ed6b2
Kevin Musanii <45618146+musanii@users.noreply.github.com>
2022-09-19 19:34:12 +0300

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,15 +13,12 @@
  *
  * Return: A pointer to the destination string @dest.
  */
-char _strcpy(char *dest, const char *src)
+char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
+	int i;
 
-	while (src[index])
-	{
-		dest[index] = src[index];
-		index++;
-	}
+	for (i = 0; i <= _strlen(src); i++)
+		dest[i] = src[i];
 
 	return (dest);
 }
