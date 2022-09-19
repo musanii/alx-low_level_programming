098f53d82775739c2414067749a4867ff1891794
Kevin Musanii <kibetmusanii@gmail.com>
2022-09-19 03:08:08 -0700

    get the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+/*
+ * File: 2-strlen.c
+ * Author: Kevin Musanii
+ */
+
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a str
+ * @str: The string to get the length of
+ * Return : The length of @str.
+ */
+size_t _strlen(const char *str)
+{
+	size_t length = 0;
+
+	while (*str++)
+		length++;
+
+	return (length);
+}
