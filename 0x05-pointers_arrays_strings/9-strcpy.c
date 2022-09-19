c305c840801dcfdce21d26505aede18c34a54649
Kevin Musanii <45618146+musanii@users.noreply.github.com>
2022-09-19 19:42:56 +0300

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,34 +13,15 @@
  *
  * Return: A pointer to the destination string @dest.
  */
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-
-	return (len);
-}
-
-/**
- * _strcpy - copies the string pointed to by src,
- * including the terminating null byte (\0),
- * to the buffer pointed to by dest
- * @dest: copy source to this buffer
- * @src: this is the source to copy
- * Return: copy of original source
- */
-
 char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for (i = 0; i <= _strlen(src); i++)
+	for (i = 0; src[i] != '\0'; i++)
+	{
 		dest[i] = src[i];
+	}
+	dest[i] = '\0';
 
 	return (dest);
 }
