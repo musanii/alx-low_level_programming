7b4c902330849e3533ba1308146b9d0f1090bc38
Kevin Musanii <45618146+musanii@users.noreply.github.com>
2022-09-23 11:03:00 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,10 +18,10 @@ char *_strncat(char *dest, char *src, int n)
 	int i = 0;
 	int j = 0;
 
-	while (dest[i] != '\0') /*i is index of null terminator*/
+	while (dest[i] != '\0')
 		i++;
 
-	while (src[j] != src[n]) /*append replacing null terminator*/
+	while (src[j] != src[n])
 	{
 		dest[i] = src[j];
 		i++;
