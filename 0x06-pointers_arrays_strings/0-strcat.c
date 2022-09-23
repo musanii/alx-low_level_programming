3ba0be488c7b7bf43f14df44275c83440b1f95bb
Kevin Musanii <kibetmusanii@gmail.com>
2022-09-22 22:03:52 -0700

    Initial commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,27 +7,21 @@
 
 /**
  * strcat - Concatenates the string pointed to by @src, including the terminating
- * null byte, to the end of the string pointed to by @dest.
+ * 		null byte, to the end of the string pointed to by @dest.
  * @dest: A pointer to the string to be concatnated upon.
  *
- * Return: A pointer to the destination string @dest.
+ * Return: A pointer to the destination string @dest
  */
-
-char *_strcat(char *dest, char *src)
+char *strcat(char *dest, const char *src)
 {
-	int i = 0;
-	int j = 0;
+	int index = 0;
+	int dest_len = 0;
 
-	while (dest[i] != '\0')
-		i++;
+	while (dest[index++])
+		dest_len++;
 
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
