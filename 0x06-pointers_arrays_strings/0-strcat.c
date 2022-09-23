9099a4d4ba92ae770ebff5f697412dbe796951b3
Kevin Musanii <kibetmusanii@gmail.com>
2022-09-22 22:57:45 -0700

    Initial commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -18,17 +18,16 @@ char *strcat(char *dest, char *src)
 	int i = 0;
 	int j = 0;
 
-	while (*(dest + i))
+	while (dest[i] != '\0')
 		i++;
 
-	while (*(src + j))
+	while (src[j] != '\0')
 	{
-		*(dest + i) = *(src + j);
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
-	*(dest + i) = '\0';
+	dest[i] = '\0';
 
 	return (dest);
 }
-	
