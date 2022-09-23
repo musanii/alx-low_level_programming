fd9ec13d61d08398ed742cc883e5767c06599b1b
Kevin Musanii <45618146+musanii@users.noreply.github.com>
2022-09-23 11:53:59 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,14 +8,8 @@
 
 char *string_toupper(char *s)
 {
-
-	int i = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] >= 'a' && s[i] <= 'z')
-			s[i] = s[i] - 'a' + 'A';
-	}
-	
-	return (s);
+        while (*s)
+	    if (*s >= 'a' && *s <= 'z')
+	        *s = *s -'a' + 'A';
+	    s++;
 }
