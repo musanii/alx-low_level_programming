bcdbb93105b78e4fc651a697deffe777b5bdb009
Kevin Musanii <45618146+musanii@users.noreply.github.com>
2022-09-23 11:38:22 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * string_toupper - capitalize all letters in string
- * @s: string to manipulate
- * Return: string with all letters capitalized
+ * string_toupper - capitalize all letters in string.
+ * @s: string to manipulate.
+ * Return: string with all letters capitalized.
  */
 
 char *string_toupper(char *s)
