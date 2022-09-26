88bf68e2f111535ed863ad82b978fa76024f7a13
Kevin Musanii <45618146+musanii@users.noreply.github.com>
2022-09-26 22:17:24 +0300

    Update 100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,14 +2,14 @@
 
 /**
 * set_string - function that sets the value of a pointer to a char.
-* @s: source for copy
+* @s: source for copy.
 * @to: string to copy to.
 *
-* Return: Nothing
+* Return: Nothing.
 */
 
 void set_string(char **s, char *to)
 {
-     *s = to;
+        *s = to;
 }
 
