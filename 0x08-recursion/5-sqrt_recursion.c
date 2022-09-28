3168f6c76c5154382f329f24b3d25fe68664e8f4
Kevin Musanii <kibetmusanii@gmail.com>
2022-09-28 16:37:23 -0700

    find natural square root or fail with -1

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,40 @@
+/*
+ * File: 5-sqrt_recursion.c
+ * Author: Kevin Musanii
+ */
+
+#include "main.h"
+
+/**
+ * find_sqroot - prints the squareroot of anumber.
+ * @n: number or integer.
+ * @root: test the root.
+ *
+ * Return: natural square root or -1 if not natural.
+ */
+
+int find_sqroot(int n int root)
+{
+	if ( root * root > n)
+		return (-1);
+
+	if ( root * root == n)
+		return (root);
+
+	return (find_sqroot(n, root + 1));
+}
+
+/**
+ * _sqrt_recursion - find natural square root of n.
+ * @n: number
+ *
+ * Return:  natural square root or -1 if otherwise.
+ */
+
+int _sqrt_recursion(int n)
+{
+	if ( n < 0)
+		return (-1);
+	
+	return (find_sqroot(n, 0));
+}
