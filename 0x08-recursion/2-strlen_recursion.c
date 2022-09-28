8792c55c420f6ea8d1265237e862469b728c4a65
Kevin Musanii <kibetmusanii@gmail.com>
2022-09-28 16:03:49 -0700

    return a string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+/*
+ * File: 2-strlen_recursion.c
+ * Author: Kevin Musanii
+ */
+
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: string.
+ *
+ * Return: string length.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+
+	s++;
+	return (1 + _strlen_recursion(s));
+}
