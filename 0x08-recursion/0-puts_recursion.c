ebcaa50675866324f43407e829ccc3f3527c2291
Kevin Musanii <kibetmusanii@gmail.com>
2022-09-28 15:34:53 -0700

    typo fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,7 +9,7 @@
  * _puts_recursion - Prints a string, followed by a new line.
  * @4: The string to be printed.
  */
-void _puts_recusrion(char *s)
+void _puts_recursion(char *s)
 {
 	if(*s == '\0')
 	{
