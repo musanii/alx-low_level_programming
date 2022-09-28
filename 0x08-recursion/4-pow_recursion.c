cc546262de3a43b5427a66578d5434d3a95f8711
Kevin Musanii <kibetmusanii@gmail.com>
2022-09-28 16:24:17 -0700

    exponential of a number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+/*
+ * File: 4-pow_recursion.c
+ * Author: Kevin Musanii
+ */
+
+#include "main.h"
+
+/**
+ * _pow_recursion - raise x power y.
+ * @x: number.
+ * @y: Exponent to raise to
+ *
+ *Return: Power of a number, -1 if power is less than 0.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (x == 0)
+		return (0);
+	if (x == 1 || y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y, -1));
+}
