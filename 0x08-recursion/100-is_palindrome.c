f97e394892f6ffd034a2108be3ffbfc553cc856f
Kevin Musanii <kibetmusanii@gmail.com>
2022-09-28 17:18:13 -0700

    palindrome numer generator

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,16 +6,15 @@
 #include "main.h"
 
 /**
- * compare - compare head and tail indices for match.
- * @head: index starting from left of string.
- * @tail: index starting from right of string, moving backwards.
+ * compare - compare head and tail indices for match
+ * @head: index starting from left of string
+ * @tail: index starting from right of string, moving backwards
  *
  * Return: 1 if palindrome, 0 if not
  */
 
 int compare(char *head, char *tail)
 {
-
 	if (head >= tail)
 		return (1);
 	if (*head == *tail)
@@ -25,23 +24,27 @@ int compare(char *head, char *tail)
 }
 
 /**
- * _strlen - find length of string to access last index
+ * _strlen - finds length of string to access last index.
  * @s: string
- * Return: length
+ *
+ * Return: Length
  */
+
 int _strlen(char *s)
 {
-
 	if (*s == '\0')
 		return (0);
-	s++;
-	return (1 + (_strlen(s)));
+		s++;
+		return (1 + (_strlen(s)));
 }
+
 /**
- * is_palindrome - check if palindrome
+ * is_palindrome - check if palindrome.
  * @s: string to check
- * Return: 1 if palindrome, 0 if not
+ *
+ * Return: 1 if palindrome, 0 if not.
  */
+
 int is_palindrome(char *s)
 {
 	int len = _strlen(s);
