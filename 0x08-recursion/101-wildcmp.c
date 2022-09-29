5cc2441a14e3fd4de64cf6e1cd6a2b3359f14fac
Kevin Musanii <kibetmusanii@gmail.com>
2022-09-28 17:35:37 -0700

    compare if string with wildcard mattches

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,15 +1,24 @@
+/*
+ * File: 101-wildcmp.c
+ * Author: Kevin Musanii
+ */
+
+#include "main.h"
+
 /**
- * substring_match - check if a substring after wildcard matches s1
- * @s1: one string
- * @s2: one string
- * @after_wldcd: placeholder for position right after wildcard
- * Return: 1 if matched, 0 if not
+ * substring_match - check if a substring after wildcard matches s1.
+ * @s1: one string.
+ * @s2: one string.
+ * @after_wldcd: placeholder for position right after wildcard.
+ * 
+ * Return: 1 if matched, 0 if not.
  */
 
 int substring_match(char *s1, char *s2, char *after_wldcd)
 {
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
+		
 	if (*s1 == '\0' && *s2 == '*')
 		return (substring_match(s1, s2 + 1, s2 + 1));
 	if (*s1 == '\0' && *s2 != '\0')
@@ -23,10 +32,10 @@ int substring_match(char *s1, char *s2, char *after_wldcd)
 }
 
 /**
- * wildcmp - compare if string with wildcard mattches
- * @s1: one string
- * @s2: one string
- * Return: 1 if matched, 0 if not
+ * wildcmp - compare if string with wildcard mattches.
+ * @s1: one string.
+ * @s2: one string.
+ * Return: 1 if matched, 0 if not.
  */
 
 int wildcmp(char *s1, char *s2)
