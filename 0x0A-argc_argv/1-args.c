43a26563753fbbd5d96a68ec720564b3da0a5fbb
Kevin Musanii <kibetmusanii@gmail.com>
2022-10-03 10:37:07 -0700

    function to print # of args passed into it

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,13 +7,14 @@
 #include "main.h"
 
 /**
- * main - print number of arguments passed to program
- * @argc: argument counter
- * @argv: pointer to array of arguments
- * Return: 0 on success
+ * main - print number of arguments passed to program.
+ * @argc: argument counter.
+ * @argv: pointer to array of arguments.
+ * 
+ * Return: 0 on success.
  */
 
- int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	if (argc > 0)
 		printf("%d\n", argc - 1);
