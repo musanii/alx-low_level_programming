597c10bf4889b60331d0ac2975e27437e80fefba
Kevin Musanii <45618146+musanii@users.noreply.github.com>
2022-10-03 21:29:57 +0300

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
- * main - print each argument passed to program on a line
- * @argc: argument counter
- * @argv: pointer to array of arguments
- * Return: 0 on success
+ * main - prints the number of arguments passed into the program
+ * @argc: int
+ * @argv: list
+ * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
-	int i = 0;
+int i = 0;
 
-	while (i < argc)
-		printf("%s\n", argv[i++]);
+while (argc--)
+{
+	printf("%s\n", argv[i]);
+	i++;
+}
 
-	return (0);
+return (0);
 }
