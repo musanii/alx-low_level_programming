349a206cf0ec39471bacc30465de2905d652a340
Kevin Musanii <45618146+musanii@users.noreply.github.com>
2022-10-03 21:44:58 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,23 +12,23 @@
 int main(int argc, char *argv[])
 {
 	int n, coins = 0;
-
+	
 	/* validate input */
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
+	
 	if (argv[1][0] == '-')
 	{
 		printf("0\n");
 		return (0);
 	}
-
+	
 	/* convert string to int and calculate coins */
 	n = atoi(argv[1]);
-
+	
 	coins += n / 25;
 	n = n % 25;
 	coins += n / 10;
@@ -38,7 +38,7 @@ int main(int argc, char *argv[])
 	coins += n / 2;
 	n = n % 2;
 	coins += n / 1;
-
+	
 	printf("%d\n", coins);
 	return (0);
 }
