eb31c68b64ab6d38a2ad6213796a4c09b556d9a1
Kevin Musanii <45618146+musanii@users.noreply.github.com>
2022-10-03 21:25:06 +0300

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,31 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
+#include <stdio.h> /* printf */
+#include <stdlib.h> /* atoi */
+#include <stdbool.h> /* bool data type */
 
 /**
- * is_num - iterate through each argv to test if it's a number.
- * @argvv: a argvv
- *
- * Return: true iff entire string is a number.
+ * is_num - iterate through each argv to test if it's a number
+ * @argvv: a argv
+ * Return: true only if entire string is a number, false if not
  */
+
 bool is_num(char *argvv)
 {
 	int j = 0;
 
 	for (j = 0; argvv[j]; j++)
 	{
-		if ( !(agrvv[j] >= '0' && argvv[j] <= '9'))
+		if (!(argvv[j] >= '0' && argvv[j] <= '9'))
 			return (0);
 	}
 	return (1);
 }
 
 /**
- * main - print sum if all args given are numbers
+ * main - print sum if all arguments given are numbers
  * @argc: argument counter
  * @argv: arguments
- *
- * Return: 0 on success , 1 if an argument wasnt a number.
+ * Return: 0 on success, 1 if an argument wasn't a number
  */
 
 int main(int argc, char *argv[])
@@ -33,15 +32,17 @@ int main(int argc, char *argv[])
 	int i = 1;
 	int sum = 0;
 
-	if (argc == 1) 
+	/* validate input */
+	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
 	}
 
+	/* check all arguments to add numbers */
 	while (i < argc)
 	{
-		if (is_num(argv[1]))
+		if (is_num(argv[i]))
 			sum += atoi(argv[i]);
 		else
 		{
@@ -51,6 +52,6 @@ int main(int argc, char *argv[])
 		i++;
 	}
 	printf("%d\n", sum);
-	
+
 	return (0);
 }
