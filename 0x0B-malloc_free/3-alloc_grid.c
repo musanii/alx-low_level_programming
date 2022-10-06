35de4aafa19dfbf7694d2dbc46a83b6db503b73e
Kevin Musanii <45618146+musanii@users.noreply.github.com>
2022-10-06 05:54:38 +0300

    a function that returns a pointer to a 2 dimensional array of integers.

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,27 +11,27 @@
 
 char *_strdup(char *str)
 {
-		char *duplicate_str;
-			int i = 0, len = 0;
+	char *duplicate_str;
+	int i = 0, len = 0;
 
-				if (str == NULL) /* validate str input */
-							return (NULL);
+	if (str == NULL) /* validate str input */
+		return (NULL);
 
-					while (*(str + i))
-								len++, i++;
-						len++; /* add null terminator to length */
+	while (*(str + i))
+		len++, i++;
+	len++; /* add null terminator to length */
 
-							duplicate_str = malloc(sizeof(char) * len); /* allocate memory */
+	duplicate_str = malloc(sizeof(char) * len); /* allocate memory */
 
-								if (duplicate_str == NULL) /* validate memory */
-											return (NULL);
+	if (duplicate_str == NULL) /* validate memory */
+		return (NULL);
 
-									i = 0;
-										while (i < len)
-												{
-															*(duplicate_str + i) = *(str + i);
-																	i++;
-																		}
+	i = 0;
+	while (i < len)
+	{
+		*(duplicate_str + i) = *(str + i);
+		i++;
+	}
 
-											return (duplicate_str);
+	return (duplicate_str);
 }
